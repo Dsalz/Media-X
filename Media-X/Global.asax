42cec784c30632f7460ffa89fa6e21d432dbd8e1
damola salisu <salisu.damola@yahoo.com>
2018-06-21 15:42:45 +0100

    Add project files.

---
+++ Media-X/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Media_X.MvcApplication" Language="C#" %>
---
+++ Media-X/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Media_X.Startup))]
+namespace Media_X
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
